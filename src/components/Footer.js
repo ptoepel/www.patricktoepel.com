9deb08a775dc7ad6aafb2296715dd18e84fb9515
Patrick Toepel <patrick.toepel@gmail.com>
2023-11-25 14:20:38 -0700

    footer style change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
 
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
+        <div className="content has-text-centered">
           <div className="container">
             <div style={{ maxWidth: "100vw" }} className="columns">
               <div className="column is-4">
